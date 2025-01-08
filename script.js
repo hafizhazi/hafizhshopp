bdf30ac470b06f7b9f62312fe2e77dfb8f66a957
hafizhazi <hafizhazi2009@gmail.com>
2025-01-08 18:48:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Fungsi untuk menambahkan produk ke keranjang
+function addToCart(productName, price) {
+    alert(`Produk ${productName} dengan harga Rp${price.toLocaleString()} telah ditambahkan ke keranjang!`);
+}
